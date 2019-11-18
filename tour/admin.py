59f511539a0d5681e4bd88c8d7ee26ac4e5fff2d
Kali Junior Brown <browndesmond30@yahoo.com>
2019-11-18 13:46:46 +0000

    Kali: Models Complete

--- tour/admin.py
+++ tour/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from tour.models.category import Category
 from tour.models.employee import Employee
+from tour.models.trip import Trip
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Employee)
+admin.site.register(Trip)
--- tour/models/trip.py
+++ tour/models/trip.py
@@ -1,6 +1,8 @@
 from django.db import models 
 
+from tour.models.employee import Employee
 
 class Trip(models.Model):
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=500)
+    employees = models.ManyToManyField(Employee)
