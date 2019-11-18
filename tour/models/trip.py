c2d64c2d2bbdeb21e2d44b66b55ed6fc76d4496c
Kali Junior Brown <browndesmond30@yahoo.com>
2019-11-18 17:51:52 +0000

    Kali: Str method added

--- tour/models/trip.py
+++ tour/models/trip.py
@@ -6,3 +6,6 @@ class Trip(models.Model):
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=500)
     employees = models.ManyToManyField(Employee)
+
+    def __str__(self):
+        return self.name
