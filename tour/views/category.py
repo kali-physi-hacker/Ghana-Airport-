00c143e660342cbb00150c2570437ed8d6a63a1f
Kali Junior Brown <browndesmond30@yahoo.com>
2019-11-19 09:44:16 +0000

    Kali: Create, List and Edit Functionalities added to each View Function

--- tour/views/category.py
+++ tour/views/category.py
@@ -1,10 +1,53 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 
 from tour.models.category import Category
+from tour.forms.category import CategoryForm
 
 
 def list_categories(request):
     template = "category/list.html"
     categories = Category.objects.all()
-    context = {"categories": categories}
+    context = {"categories": categories, "list_category_active": "active", "category_show": "show",
+               "category_active": "active"}
     return render(request, template, context)
+
+
+def add_category(request):
+    template = "category/add.html"
+    form = CategoryForm()
+    context = {"form": form, "add_category_active": "active", "category_show": "show", "category_active": "active"}
+    return render(request, template, context)
+
+
+def create_category(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Category Created Successfully")
+            return redirect("list_categories")
+        else:
+            messages.error(request, "Category Creation Failed!")
+            return redirect("add_category")
+
+
+def edit_category(request, pk):
+    template = "category/edit.html"
+    category = get_object_or_404(Category, pk=pk)
+    form = CategoryForm(request.GET or None, instance=category)
+    context = {"form": form}
+    return render(request, template, context)
+
+
+def update_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == "POST":
+        form = CategoryForm(request.POST or None, instance=category)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Category Update Successfully")
+            return redirect("list_categories")
+        else:
+            messages.error(request, "Category Update Failed!")
+            return redirect("edit_category")
--- tour/views/employee.py
+++ tour/views/employee.py
@@ -1,10 +1,57 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 
 from tour.models.employee import Employee
+from tour.models.category import Category
+from tour.forms.employee import EmployeeForm
 
 
 def list_employees(request):
     template = "employees/list.html"
     employees = Employee.objects.all()
-    context = {"employees": employees}
+    context = {"employees": employees, "list_employee_active": "active", "employee_show": "show",
+               "employee_active": "active"}
     return render(request, template, context)
+
+
+def add_employee(request):
+    template = "employees/add.html"
+    form = EmployeeForm()
+    categories = Category.objects.all()
+    context = {"form": form, "categories": categories, "add_employee_active": "active", "employee_show": "show",
+               "employee_active": "active"}
+    return render(request, template, context)
+
+
+def create_employee(request):
+    if request.method == "POST":
+        form = EmployeeForm(request.POST or None)
+        # import pdb; pdb.set_trace()
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Employee Added Successfully")
+            return redirect("list_employees")
+        else:
+            messages.error(request, "Employee Creation Failed!")
+            return redirect("add_employee")
+
+
+def edit_employee(request, pk):
+    template = "employees/edit.html"
+    employee = get_object_or_404(Employee, pk=pk)
+    form = EmployeeForm(request.GET or None, instance=employee)
+    context = {"form": form}
+    return render(request, template, context)
+
+
+def update_employee(request, pk):
+    if request.method == "POST":
+        employee = get_object_or_404(Employee, pk=pk)
+        form = EmployeeForm(request.POST or None, instance=employee)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Employee Information Updated Successfully")
+            return redirect("list_employees")
+        else:
+            messages.error(request, "Employee Update Failed")
+            return redirect("edit_employee")
