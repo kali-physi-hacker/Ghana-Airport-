2ca54ef248c7b1122fbc1cc734f5b3c3e8e179a6
Kali Junior Brown <browndesmond30@yahoo.com>
2019-11-19 09:44:59 +0000

    Kali: STATUS Message Configuration added to settings

--- travel/settings.py
+++ travel/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from django.contrib import messages
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -120,4 +122,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
-print(STATICFILES_DIRS)
+
+
+# MESSAGES
+MESSAGE_TAGS = {
+    messages.DEBUG: "alert-info",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger"
+}
